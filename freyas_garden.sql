0ff629a960a3e1ac124bb9cad3ec1ad5188403b7
cerberus2477 <tabor.tunde@gmail.com>
2024-10-04 21:17:25 +0200

    tigrincs sql-jét hozzáadtam amiben vannak már rangok pl

--- freyas_garden.sql
+++ freyas_garden.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Sze 17. 12:19
+-- Létrehozás ideje: 2024. Sze 27. 13:29
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `freyas_garden`
 --
-CREATE DATABASE IF NOT EXISTS `freyas_garden` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `freyas_garden`;
 
 -- --------------------------------------------------------
 
@@ -29,13 +27,12 @@ USE `freyas_garden`;
 -- Tábla szerkezet ehhez a táblához `article`
 --
 
-CREATE TABLE IF NOT EXISTS `article` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
   `title` tinytext NOT NULL COMMENT 'A cikk címe',
   `plant_id` int(11) DEFAULT NULL COMMENT 'plant tábla ideken kulcsa (alapértelmezett null)',
-  `source` text NOT NULL COMMENT 'A cikk forrásának helye',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE= utf8mb4_unicode_ci;
+  `source` text NOT NULL COMMENT 'A cikk forrásának helye'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -43,12 +40,11 @@ CREATE TABLE IF NOT EXISTS `article` (
 -- Tábla szerkezet ehhez a táblához `plant`
 --
 
-CREATE TABLE IF NOT EXISTS `plant` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `plant` (
+  `id` int(11) NOT NULL,
   `name` int(11) NOT NULL,
-  `latin_name` int(11) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE= utf8mb4_unicode_ci;
+  `latin_name` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -56,17 +52,36 @@ CREATE TABLE IF NOT EXISTS `plant` (
 -- Tábla szerkezet ehhez a táblához `post`
 --
 
-CREATE TABLE IF NOT EXISTS `post` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `city` tinytext NOT NULL,
   `title` tinytext NOT NULL,
   `plant` int(11) NOT NULL,
   `description` text NOT NULL,
   `media` int(11) NOT NULL COMMENT 'képek helye? még nem végleges',
-  `sell` tinyint(1) NOT NULL COMMENT '0 = vétel, 1 = eladás',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE= utf8mb4_unicode_ci;
+  `sell` tinyint(1) NOT NULL COMMENT '0 = vétel, 1 = eladás'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `roles`
+--
+
+INSERT INTO `roles` (`id`, `role_name`) VALUES
+(4, 'admin'),
+(5, 'stats'),
+(6, 'user');
 
 -- --------------------------------------------------------
 
@@ -74,15 +89,16 @@ CREATE TABLE IF NOT EXISTS `post` (
 -- Tábla szerkezet ehhez a táblához `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` tinytext NOT NULL,
   `email` tinytext NOT NULL,
   `city` tinytext NOT NULL,
   `birthdate` date NOT NULL,
   `password` varchar(255) NOT NULL COMMENT 'hashelt jelszó',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE= utf8mb4_unicode_ci;
+  `role_id` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -90,15 +106,90 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Tábla szerkezet ehhez a táblához `user_plants`
 --
 
-CREATE TABLE IF NOT EXISTS `user_plants` (
+CREATE TABLE `user_plants` (
   `user_id` int(11) NOT NULL,
-  `plant_id` int(11) NOT NULL,
-    PRIMARY KEY (`user_id`, `plant_id`),
-    KEY `user_id` (`user_id`),
-   KEY `plant_id` (`plant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE= utf8mb4_unicode_ci;
+  `plant_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Indexek a kiírt táblákhoz
+--
 
+--
+-- A tábla indexei `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_article_plant` (`plant_id`);
+
+--
+-- A tábla indexei `plant`
+--
+ALTER TABLE `plant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_post_user` (`user_id`);
+
+--
+-- A tábla indexei `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `role_name` (`role_name`);
+
+--
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- A tábla indexei `user_plants`
+--
+ALTER TABLE `user_plants`
+  ADD PRIMARY KEY (`user_id`,`plant_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `plant_id` (`plant_id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `plant`
+--
+ALTER TABLE `plant`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Megkötések a kiírt táblákhoz
@@ -115,6 +206,12 @@ ALTER TABLE `article`
 --
 ALTER TABLE `post`
   ADD CONSTRAINT `fk_post_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Megkötések a táblához `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 
 --
 -- Megkötések a táblához `user_plants`
